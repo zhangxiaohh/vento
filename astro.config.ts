197b3e724cd43d9fc521668785a5fe11b76a4b62
vento <210567852@qq.com>
2026-04-13 11:00:38 +0800

    feat: 改一下网站的名字

--- astro.config.ts
+++ astro.config.ts
@@ -13,7 +13,7 @@ import {
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
-  base: "/Myposts/",
+  base: "/vento/",
   i18n: {
     locales: SUPPORTED_LOCALES,
     defaultLocale: DEFAULT_LOCALE,
